be9b88c342004a9a815fe1770b1d28f955db1355
Aryan Soni <arpitsoni811@gmail.com>
2023-10-31 01:14:40 +0530

    unable to post

--- index.js
+++ index.js
@@ -1,51 +1,72 @@
-let maxWord;
-fetch("https://one00x-data-analysis.onrender.com/assignment")
-  .then((response) => {
-    if (response.ok) {
-      console.log("Call Successful.");
-      return response.json();
-    } else console.error("Call Not Successful");
-  })
-  .then((data) => {
-    console.log(data.sort());
-    let count = 0,
-      maxCount = 0;
+const getData = async () => {
+  await fetch(
+    "https://one00x-data-analysis.onrender.com/assignment?email=arpitsoni811@gmail.com"
+  )
+    .then((response) => {
+      if (response.ok) {
+        console.log("Call Successful.");
+        return response.json();
+      } else console.error("Call Not Successful");
+    })
+    .then((data) => {
+      //   console.log(data.sort());
+      let maxWord,
+        count = 0,
+        maxCount = 0;
 
-    for (let i = 0; i < 99; i++) {
-      if (data[i] === data[i + 1]) {
-        count++;
-      } else {
-        if (maxCount < count) {
-          maxCount = count;
-          maxWord = data[i - 1];
+      for (let i = 0; i < 99; i++) {
+        if (data[i] === data[i + 1]) {
+          count++;
+        } else {
+          if (maxCount < count) {
+            maxCount = count;
+            maxWord = data[i - 1];
+          }
+          count = 0;
         }
-        count = 0;
       }
-    }
-    console.log(maxWord);
-  });
-fetch("https://one00x-data-analysis.onrender.com/assignment", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify({ maxWord }),
-})
-  .then((response) => {
-    if (response.ok) {
-      return response.json(); // Parse response as JSON
-    } else {
-      throw new Error("Response posting failed"); // Handle failure
-    }
-  })
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((error) => {
-    // Handle error
-    console.log(`Error: ${error.message}`);
-  });
+      console.log(maxWord);
+      return maxWord;
+    });
+};
 
+const postData = async (result) => {
+  fetch(
+    "https://one00x-data-analysis.onrender.com/assignment?email=arpitsoni811@gmail.com",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(result),
+    }
+  )
+    .then((response) => {
+      if (response.ok) {
+        return response.json(); // Parse response as JSON
+      } else {
+        throw new Error("Response posting failed"); // Handle failure
+      }
+    })
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      // Handle error
+      console.log(`Error: ${error.message}`);
+    });
+};
+
+const getThenPost = async () => {
+  let maxWord = await getData();
+  let result = {
+    word: maxWord,
+    assignmentId: "dfd1ac15-0ca3-4b23-af8b-5a5b44c6c0a2",
+  };
+  await postData(result);
+};
+
+getThenPost();
 // x-assignment-id : dfd1ac15-0ca3-4b23-af8b-5a5b44c6c0a2
 
 // async function fetchData() {
